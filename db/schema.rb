f65fde1d6ecc800c3017e6eb5a8c6a6d1e8a6cb8
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-11-22 16:50:20 +0400

    Change default devise controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_111259) do
+ActiveRecord::Schema.define(version: 2021_11_22_112705) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
